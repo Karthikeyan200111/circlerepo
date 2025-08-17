edcf9ac725ab19c7c7c7f8aa03e41f0863838482
Karthikeyan200111 <kannankarthi737@gmail.com>
2025-08-17 18:21:03 +0530

    changed the backenf

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import json
+import uvicorn
 
 app = FastAPI()
 
@@ -45,3 +46,7 @@ async def websocket_endpoint(websocket: WebSocket):
 
     except WebSocketDisconnect:
         active_connections.remove(websocket)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
